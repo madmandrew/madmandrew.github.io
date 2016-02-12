aa1db2da2e6bc8d1710a9379dfafaadcac68838a
Andrew Barta <andrew@idahobartas.com>
2016-02-12 10:53:20 -0700

    ajax test2

--- html/citPortfolio/portfolio_js/ajax_js.js
+++ html/citPortfolio/portfolio_js/ajax_js.js
@@ -2,22 +2,50 @@ function ajaxRequest () {
 
     console.log("start");
 
-    var request = new XMLHttpRequest();
+     var urlString = "https://maps.googleapis.com/maps/api/place/autocomplete/json?" +
+    "input=Boi" +
+    "&types=(cities)&key=AIzaSyBTO-OBrlm2zzyqu__C60vegyr_0Yu0tr4";
+    //var urlString = "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=bois";
+
+    var request = createCORSRequest('GET', urlString);
+    request.setRequestHeader('Authorization', 'Bearer AIzaSyBTO-OBrlm2zzyqu__C60vegyr_0Yu0tr4');
+
+    if (!request)
+    {
+        console.log("error creating request");
+        return;
+    }
+
+    console.log(request);
+
+    request.onloadstart = function() {
+     console.log("started");
+    }
+    console.log("before load");
+    request.onload = function() {
+        console.log("loaded");
+        console.log(request.responseText);
+        console.log(request);
+    }
+
+    request.send();
+
+
+
+
 
     //request.onreadystatechange = ajaxCallback(request);
 
-    var urlString = "https://maps.googleapis.com/maps/api/place/autocomplete/json?" +
-    "input=Boi" +
-    "&types=(cities)&key=AIzaSyBTO-OBrlm2zzyqu__C60vegyr_0Yu0tr4";
 
-    console.log("open request");
+
+    /*console.log("open request");
     request.open("GET", urlString, false);
     request.send();
 
 
     console.log("callback function");
     console.log(request);
-    console.log(request.responseText);
+    console.log(request.responseText);*/
 }
 
 var ajaxCallback = function(response)
@@ -25,4 +53,31 @@ var ajaxCallback = function(response)
     console.log("callback function");
     console.log(response);
     console.log(response.responseText);
+}
+
+
+
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
 }
